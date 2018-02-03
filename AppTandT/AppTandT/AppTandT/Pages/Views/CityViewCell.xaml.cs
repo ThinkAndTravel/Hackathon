ef1d90993fed2bd92598938a6e364a7829701e68
Slawik Iwanenko <good.worker.1997@gmail.com>
2018-02-03 18:05:10 +0200

    add pages

--- AppTandT/AppTandT/AppTandT/Pages/Views/CityViewCell.xaml.cs
+++ AppTandT/AppTandT/AppTandT/Pages/Views/CityViewCell.xaml.cs
@@ -10,11 +10,36 @@ using Xamarin.Forms.Xaml;
 namespace AppTandT.Pages.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class CityViewCell : ContentView
+    public partial class CityViewCell : ViewCell
     {
         public CityViewCell()
         {
             InitializeComponent();
+
+            /*View.GestureRecognizers.Add(new TapGestureRecognizer()
+            {
+                Command = new BaseCommand((arg) =>
+                {
+                    var item = BindingContext as CityItem;
+
+
+
+                    //тут потрібно змінювати місто
+                    System.Diagnostics.Debug.WriteLine("CITY TAPPED");
+                })
+            });*/
+        }
+
+        protected override void OnBindingContextChanged()
+        {
+            base.OnBindingContextChanged();
+
+            var item = BindingContext as CityItem;
+            if (item == null)
+                return;
+            LogoView.Source = item.LogoUrl;
+            NameView.Text = item.Name;
+
         }
     }
 }
---
+++ AppTandT/AppTandT/AppTandT/Pages/Views/TaskViewCell.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppTandT.Pages.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TaskViewCell : ViewCell
+    {
+        public TaskViewCell()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnBindingContextChanged()
+        {
+            base.OnBindingContextChanged();
+
+            var item = BindingContext as TaskItem;
+            if (item == null)
+                return;
+            LogoView.Source = item.LogoUrl;
+            Title.Text = item.Title;
+            About.Text = item.About;
+
+        }
+    }
+}
