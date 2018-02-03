6f16cb26ee4da26779e8266ce57189c4ca45bfa0
Slawik Iwanenko <good.worker.1997@gmail.com>
2018-02-03 19:10:06 +0200

    Add Post and identity service

---
+++ AppTandT/AppTandT/AppTandT/BLL/Services/IdentityService.cs
@@ -0,0 +1,79 @@
+﻿using AppTandT.BLL.Help;
+using AppTandT.BLL.Model;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppTandT.BLL.Services
+{
+    public class IdentityService : ClientService
+    {
+
+        class jsonLoginModel
+        {
+            public string username { get; set; }
+
+            public string access_token { get; set; }
+        };
+
+        /// <summary>
+        /// підключається до сервера та робить логін для юзера
+        /// отримує JWT, та зберігає
+        /// </summary>
+        /// <param name="loginModel"></param>
+        /// <returns>
+        /// string => опис помилки
+        /// null => коли все вірно
+        /// </returns>
+        public static async Task<string> LoginAsync(LoginModel loginModel)
+        {
+
+            var client = GetClient();
+            var response = await client.PostAsync(App.ApiUri + "api/auth/token",
+               new StringContent(
+                    JsonConvert.SerializeObject(loginModel),
+                    Encoding.UTF8, "application/json")
+                );
+            var jwt = await response.Content.ReadAsStringAsync();
+            if (response.StatusCode != HttpStatusCode.OK)
+                throw new ServiceException(jwt);
+
+
+            jsonLoginModel model = JsonConvert.DeserializeObject<jsonLoginModel>(jwt);
+
+            Dekanat.newSesion(model.username, loginModel.Username, model.access_token);
+
+            return null;
+        }
+
+        /// <summary>
+        /// Реєструє користувача, або виводить помилку
+        /// </summary>
+        /// <returns>
+        /// string => значення помилки
+        /// null => все пройшло успішно
+        /// </returns>
+        public static async Task<string> RegistryAsync(RegistryModel registryModel)
+        {
+            var client = GetClient();
+            var response = client.PostAsync(App.ApiUri + "api/registry",
+               new StringContent(
+                    JsonConvert.SerializeObject(registryModel),
+                    Encoding.UTF8, "application/json")).Result;
+
+            var jwt = await response.Content.ReadAsStringAsync();
+
+            if (response.StatusCode != HttpStatusCode.OK)
+            {
+                var jwtobj = JsonConvert.DeserializeObject<JwtModel>(jwt);
+                throw new ServiceException(jwtobj.message);
+            }
+            return null;
+        }
+    }
+}
---
+++ AppTandT/AppTandT/AppTandT/BLL/Services/PostService.cs
@@ -0,0 +1,49 @@
+﻿using AppTandT.BLL.Help;
+using AppTandT.BLL.Model.CollectionModels;
+using AppTandT.BLL.Model.ViewModels;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppTandT.BLL.Services
+{
+    public class PostService : ClientService
+    {
+        public static async Task<List<PostViewModel>> GetPostsPageAsync(string id, int page = 0)
+        {
+            var client = GetClient();
+            var url = App.ApiUri + "api/post/getposts?id=" + id + "&k=" + page;
+            var response = await client.GetAsync(url);
+
+            if (response.StatusCode != HttpStatusCode.OK)
+                throw new ServiceException("Can't connect to the server! Try again later.", title: "Ooops");
+
+            var str = await response.Content.ReadAsStringAsync();
+            var arr = JsonConvert.DeserializeObject<List<PostViewModel>>(str);
+            return arr;
+        }
+
+
+        public static async Task<bool> AddPostAsync(Post post)
+        {
+            var client = GetClient();
+            var url = App.ApiUri + "api/post/addpost";
+            var response = await client.PostAsync(url, new StringContent(
+                    JsonConvert.SerializeObject(post),
+                    Encoding.UTF8,
+                    "application/json")
+            );
+
+            if (response.IsSuccessStatusCode) return true;
+
+            var str = await response.Content.ReadAsStringAsync();
+            var obj = JsonConvert.DeserializeObject<JwtModel>(str);
+            throw new ServiceException(obj.message);
+        }
+    }
+}
