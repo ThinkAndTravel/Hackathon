080fe616d4ada9ca12e2c11ee98ddc2cb26f5105
Max <moskal.max.97@gmail.com>
2018-02-04 02:15:20 +0200

    Plan AC

---
+++ AppTandT/AppTandT/AppTandT/Pages/PlanPages/AddPlanPageModel.cs
@@ -0,0 +1,24 @@
+﻿using AppTandT.BLL.Services;
+using AppTandT.Pages.Menu;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+using Xamvvm;
+using AppTandT.BLL;
+using AppTandT.BLL.Model.CollectionModels.HelpCollectionModels;
+
+namespace AppTandT.Pages.PlanPages
+{
+    public class AddPlanPageModel : BasePageModel
+    {
+        public AddPlanPageModel()
+        {
+        }
+    }
+}
